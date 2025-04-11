1cdbd004d3947c44c525797e06d36b2df9b69843
Kirill Ziuzin <kirill.zak@gmail.com>
2025-04-11 15:28:45 +0300

    master Add env value for brew

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -32,6 +32,9 @@ PKG_CASK_LIST=(
 if test ! $(which brew); then
     echo "Installing homebrew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew update
